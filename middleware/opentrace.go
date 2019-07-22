e6be4db9ddf85c55b7e6d85c67499321a65f3174
Long Nguyen <longnguyen@Longs-MacBook-Pro.local>
2019-07-22 18:52:12 +0700

    initcode

--- middleware/opentrace.go
+++
@@ -1,8 +0,0 @@
-package middleware
-
-import "bitbucket.lzd.co/lgo/httpclient/middleware/opentrace"
-
-// NewOpentrace create middleware for OpenTrace
-func NewOpentrace() *opentrace.Transport {
-	return opentrace.NewTransport()
-}
